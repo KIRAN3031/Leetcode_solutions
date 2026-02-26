121f1c3753eb026f023507fbb79fdc416c944748
kiran <chkiran123h@gamil.com>
2026-02-26 14:36:20 +0530

    Initial commit

---
+++ Difficulty/easy/1-two-sum.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return [-1, -1]  # Return an invalid index if no solution is found
+        
+# @lc code=end
+
